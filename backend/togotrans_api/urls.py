e7fb23a044a469f62a76349f4fe0e0f5848bc3d8
wil <156604641+essodond@users.noreply.github.com>
2025-09-23 16:50:43 +0000

    work3

--- backend/togotrans_api/urls.py
+++ backend/togotrans_api/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.authtoken.views import obtain_auth_token
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,7 +33,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/login/', obtain_auth_token),
     path('api/', include('transport.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
